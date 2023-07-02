55b898209ccc5a46594234eb1afc8c84da3def4b
Niyati Nehal <niyatinehal58@gmail.com>
2023-07-02 17:03:30 +0530

    added bookmark.css

--- src/Pages/BookmarkPage/Bookmark.jsx
+++ src/Pages/BookmarkPage/Bookmark.jsx
@@ -2,49 +2,80 @@ import React, { useContext } from "react";
 import { MainContext } from "../../services/contexts/MainContext";
 import { PostContext } from "../../services/HandlerContext/PostsFunc";
 import { Suggestions } from "../../components/Suggestions/Suggestions";
+import "./bookmark.css";
+import { SideBar } from "../../components/SideBar/Sidebar";
+import { Box } from "@chakra-ui/react";
+import { UserContext } from "../../services/HandlerContext/UserFunc";
 
 export const Bookmark = () => {
   const { mainState } = useContext(MainContext);
-  const{removeBookmark,checkBookmark,likeHandler,
+  const {
+    removeBookmark,
+    checkBookmark,
+    likeHandler,
     dislikeHandler,
-    checkLikes,}=useContext(PostContext);
+    checkLikes,
+  } = useContext(PostContext);
+  const { avatar } = useContext(UserContext);
 
-    const bookmarkData=[...mainState.bookMark]
-  
-  const removeHandler=(postId)=>{
-    if(checkBookmark().includes(postId)){
-      removeBookmark(postId)
-    } 
-  }
-  
+  const bookmarkData = [...mainState.bookMark];
+
+  const removeHandler = (postId) => {
+    if (checkBookmark().includes(postId)) {
+      removeBookmark(postId);
+    }
+  };
+
   console.log(bookmarkData);
   return (
-    <div>
-      <div>
-        {bookmarkData ? (
-          <div>
+    <Box className="bookmark">
+      <SideBar />
+      <Box className="bookmark-content">
+        {bookmarkData.length > 0 ? (
+          <Box className="bookmark-posts-list">
             {bookmarkData.map((bmark) => (
               <li>
-                <h3>{bmark.username}</h3>
+                {console.log(bmark)}
+                <Box className="bookmark-user">
+                  <Box className="bookmark-user-info">
+                    <img src={avatar(bmark.username)} />
+                    <h3>
+                      {bmark.firstName} {bmark.lastName}
+                      <p>
+                        @{bmark.username}
+                        <p className="date">
+                          {new Date(bmark.createdAt).toDateString()}
+                        </p>
+                      </p>
+                    </h3>
+                  </Box>
+                </Box>
+
                 <p>{bmark.content}</p>
-                <button onClick={()=>removeHandler(bmark._id)}>Remove Form Bookmark</button>
-                <button  onClick={() => {
+                <button onClick={() => removeHandler(bmark._id)}>
+                  Remove Form Bookmark
+                </button>
+                <button
+                  onClick={() => {
                     checkLikes()?.includes(bmark._id) === true
-                      ? dislikeHandler(bmark._id) 
+                      ? dislikeHandler(bmark._id)
                       : likeHandler(bmark._id);
-                  }}>{checkLikes()?.includes(bmark._id) === true
+                  }}
+                >
+                  {checkLikes()?.includes(bmark._id) === true
                     ? "dislike"
-                    : "like"}</button>
+                    : "like"}
+                </button>
               </li>
             ))}
-          </div>
+          </Box>
         ) : (
-          <div>
-            <h3>No bookMarks added</h3>
-          </div>
+          <Box className="no-bookmarks">
+            <h3>No bookMarks added !!!</h3>
+          </Box>
         )}
-      </div>
-      <Suggestions/>
-    </div>
+      </Box>
+      <Suggestions />
+    </Box>
   );
 };
