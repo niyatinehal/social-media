ad6c5d9773a1428b2980de159782e437e47727a1
Niyati Nehal <niyatinehal58@gmail.com>
2023-07-10 06:28:02 +0530

    minr fix

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { mockman } from "mockman-js";
+import miragejs from "miragejs"
 
 import { makeServer } from "./server";
 
